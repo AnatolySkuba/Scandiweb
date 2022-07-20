1ea4cbbfde3cd9889f1890c97af8b23f9d406457
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-07-20 14:33:49 +0300

    'save'

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
 					Edit <code>src/App.tsx</code> and save to reload.
 				</p>
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React2
+					Learn React3
 				</a>
 			</header>
 		</div>
