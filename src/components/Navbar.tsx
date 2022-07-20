b1f62edc79d63e5462a9ff11f7d7702cde85217f
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-07-20 17:39:25 +0300

    save-my-progress

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 import { PureComponent } from "react";
 
-export class App extends PureComponent {
+export class Navbar extends PureComponent {
 	render() {
 		return <h1>Hello TS5</h1>;
 	}
 }
 
-export default App;
+export default Navbar;
