3876d43ee78f6ee0796132b12832d9e60fe7e0bf
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-09-08 19:49:09 +0300

    save-my-progress

--- src/component/App/App.jsx
+++ src/component/App/App.jsx
@@ -3,28 +3,45 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Header from "component/Header";
 
-const ProductListPage = lazy(() => import("route/ProductListPage" /* webpackChunkName: "ProductListPage" */));
-const ProductPage = lazy(() => import("route/ProductPage" /* webpackChunkName: "ProductPage" */));
-const CartPage = lazy(() => import("route/CartPage" /* webpackChunkName: "CartPage" */));
+const ProductListPage = lazy(() =>
+    import("route/ProductListPage" /* webpackChunkName: "ProductListPage" */)
+);
+const ProductPage = lazy(() =>
+    import("route/ProductPage" /* webpackChunkName: "ProductPage" */)
+);
+const CartPage = lazy(() =>
+    import("route/CartPage" /* webpackChunkName: "CartPage" */)
+);
 
 class App extends PureComponent {
-	render() {
-		return (
-			<BrowserRouter>
-				<Suspense fallback={<Header />}>
-					<Routes>
-						<Route path="/Scandiweb/" element={<Header />}>
-							<Route index element={<ProductListPage />} />
-							<Route path="category/:category" element={<ProductListPage />} />
-							<Route path="product/:productId" element={<ProductPage />} />
-							<Route path="cart" element={<CartPage />} />
-						</Route>
-						<Route path="*" element={<Navigate to="/Scandiweb/" replace={true} />} />
-					</Routes>
-				</Suspense>
-			</BrowserRouter>
-		);
-	}
+    render() {
+        return (
+            <BrowserRouter>
+                <Suspense fallback={<Header />}>
+                    <Routes>
+                        <Route path="/Scandiweb/" element={<Header />}>
+                            <Route index element={<ProductListPage />} />
+                            <Route
+                                path="category/:category"
+                                element={<ProductListPage />}
+                            />
+                            <Route
+                                path="product/:productId"
+                                element={<ProductPage />}
+                            />
+                            <Route path="cart" element={<CartPage />} />
+                        </Route>
+                        <Route
+                            path="*"
+                            element={
+                                <Navigate to="/Scandiweb/" replace={true} />
+                            }
+                        />
+                    </Routes>
+                </Suspense>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
