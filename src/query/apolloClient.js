f8701093f82dade5de5857ea5ec9685c0ad48e87
Anatoly Skuba <89809418+AnatolySkuba@users.noreply.github.com>
2022-12-10 21:46:56 +0200

    Update apolloClient.js

--- src/query/apolloClient.js
+++ src/query/apolloClient.js
@@ -1,6 +1,6 @@
 import { InMemoryCache, ApolloClient } from "@apollo/client";
 const client = new ApolloClient({
-	uri: "https://scandiwebserve.herokuapp.com/",
+	uri: "https://scandiweb-server-c53i.onrender.com",
 	cache: new InMemoryCache({
 		dataIdFromObject: o => (o._id ? `${o.__typename}:${o._id}` : null),
 	}),
